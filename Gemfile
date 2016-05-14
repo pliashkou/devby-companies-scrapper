5bc7ab7d07890202137743e6f4e57caa771ddbf1
eugen0329 <eugen0329@gmail.com>
2016-05-14 20:37:24 +0300

    Add bearable console output

--- Gemfile
+++ Gemfile
@@ -2,5 +2,7 @@ source "https://rubygems.org"
 
 
 gem 'pry'
+gem 'awesome_print'
+
 gem 'roo', '~> 2.3', '>= 2.3.2'
 gem 'mechanize', '~> 2.7', '>= 2.7.4'
--- bin/pick.rb
+++ bin/pick.rb
@@ -2,19 +2,20 @@
 require 'roo'
 require 'mechanize'
 
-
 COMPANIES_PAGE = 'https://companies.dev.by/'
 
-agent = Mechanize.new
+companies = []
 
+agent = Mechanize.new
 agent.get(COMPANIES_PAGE) do |page|
   page.search('.companies tbody tr').each do |tr|
-    title_link = tr.at('td:first-of-type a')
-    title          = title_link.text
-    employee_count = tr.at('td:nth-of-type(3)')['data']
+    company = {}
+
+    title_link  = tr.at('td:first-of-type a')
+    company[:title] = title_link.text
+    company[:employees_count] = tr.at('td:nth-of-type(3)')['data']
 
     company_page = agent.click(title_link)
-
 
     website, phone = nil, nil
     company_page.at('.sidebar-views-contacts').tap do |contacts_section|
@@ -23,31 +24,40 @@ agent.get(COMPANIES_PAGE) do |page|
 
       contacts_section.search('li').each do |li|
         case li.text
-        when /Телефон/ then phone   = li.at('span').text
-        when /Сайт/    then website = li.at('a').text
+        when /Телефон/ then company[:phone]   = li.at('span').text
+        when /Сайт/    then company[:website] = li.at('a').text
         end
       end
     end
 
-    address = nil
     company_page.at('.info-ofice').tap do |location_section|
       break if location_section.nil?
       address_span = location_section.at('.street-address')
-      address = address_span.text unless address_span.nil?
+      company[:address] = address_span.text unless address_span.nil?
     end
 
+    representatives = []
+    company_page.search('.widget-companies-agents li').each do |r|
+      representative = {}
+      representative[:fullname] = r.at('i.icon-dev-hr + a').text
 
-    company_page.search('.widget-companies-agents li').each do |representative|
-      rep_fullname = representative.at('i.icon-dev-hr + a').text
+      r.at('.a-link') do |email_link|
+        representative[:email] = email_link.nil? ? nil : email_link.text
+      end
 
-      email_link = representative.at('.a-link')
-      rep_email    = email_link.nil? ? nil : email_link.text
+      representative[:name]     = r.at('strong').text
+      representative[:position] = r.at('strong + span').text
 
-      rep_name = representative.at('strong').text
-      rep_position = representative.at('strong + span').text
-
-      phone_span = representative.at(':last-child span')
-      phone = phone_span.nil? ? nil : phone_span.text
+      r.at(':last-child span') do |phone_span|
+        representative[:phone] = phone_span.nil? ? nil : phone_span.text
+      end
+      representatives << representative
     end
+
+    company[:representatives] = representatives
+    companies << company
   end
 end
+
+require 'awesome_print'
+ap companies
